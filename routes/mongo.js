c8201bc8a707cb908abfa755443b17206c86cc8d
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2022-06-10 21:16:54 +0700

    add new items to database success

--- routes/mongo.js
+++ routes/mongo.js
@@ -3,43 +3,56 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 async function saveNewItem(item) {
-  const newItem = new Item({ 
+  const newItem = new Item({
     'description': item.description,
     'due': item.due,
-    'isDone': item.isDone 
+    'isDone': item.isDone
   });
   await newItem.save();
   console.log('(' + newItem.description + ') is saved!');
 }
 
-async function getItems() {
-  return Item.find();
+let items, Item;
+
+// async function getItems() {
+//   items = await Item.find();
+//   console.log('items are ready!')
+// }
+
+function setUpMongo() {
+  const itemSchema = new mongoose.Schema({
+    description: String,
+    due: Date,
+    isDone: Boolean,
+  }, { collection: 'items' });
+
+  Item = new mongoose.model('Item', itemSchema);
 }
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log(`Connected to Mongo!`);
-  })
-  .catch(err => {
-    console.log(`Oh no! Mongo connection error!`);
-    console.log(err);
-  })
+async function getItems() {
+  await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log(`Connected to Mongo!`);
+    })
+    .catch(err => {
+      console.log(`Oh no! Mongo connection error!`);
+      console.log(err);
+    })
+  items = await Item.find();
+}
 
-const itemSchema = new mongoose.Schema({
-  description: String,
-  due: Date,
-  isDone: Boolean,
-}, { collection: 'items' });
+router.get('/', async (req, res) => {
+  await getItems().catch(err => console.log(err));
 
-const Item = new mongoose.model('Item', itemSchema);
-
-router.get('/', (req, res) => {
-  res.send(getItems());
+  res.send(items);
+  console.log('number of uncompleted items: ' + items.length);
 });
 
 router.post('/', (req, res) => {
   res.send(req.body.description);
   saveNewItem(req.body)
 });
+
+setUpMongo();
 
 module.exports = router;
