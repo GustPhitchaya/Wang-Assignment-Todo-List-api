d05d666e63976c5d1785293e66ea43bc1217f016
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2022-06-10 16:54:42 +0700

    able to add new item to database

--- routes/mongo.js
+++ routes/mongo.js
@@ -2,32 +2,39 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-main().catch(err => err);
-
 let dogs;
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log(`Connected to Mongo!`);
-    })
-    .catch(err => {
-      console.log(`Oh no! Mongo connection error!`);
-      console.log(err);
-    })
-
-  const dogSchema = new mongoose.Schema({
-    name: String,
-  }, { collection: 'dogs' });
-
-  const Dog = new mongoose.model('Dog', dogSchema);
-  dogs = await Dog.aggregate([
-    { $project: { name: 1 } }
-  ]);
+async function saveNewItem(item) {
+  const newDog = new Dog({ "name": item.name });
+  await newDog.save();
+  console.log(newDog.name + " is saved! huff huff!");
 }
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Connected to Mongo!`);
+  })
+  .catch(err => {
+    console.log(`Oh no! Mongo connection error!`);
+    console.log(err);
+  })
+
+const dogSchema = new mongoose.Schema({
+  name: String,
+}, { collection: 'dogs' });
+
+const Dog = new mongoose.model('Dog', dogSchema);
+dogs = Dog.aggregate([
+  { $project: { name: 1 } }
+]);
 
 router.get('/', (req, res) => {
   res.send(dogs);
+});
+
+router.post('/', (req, res) => {
+  res.send(req.body.name);
+  saveNewItem(req.body)
 });
 
 module.exports = router;
