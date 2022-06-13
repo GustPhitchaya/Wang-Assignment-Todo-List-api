83774f41b3e25f89b62c37479ee52c86938d16e7
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2022-06-13 11:34:22 +0700

    split items to uncompleted and completed

--- routes/mongo.js
+++ routes/mongo.js
@@ -4,23 +4,25 @@ const mongoose = require('mongoose');
 
 async function saveNewItem(item) {
   const newItem = new Item({
+    'title': item.title,
     'description': item.description,
     'due': item.due,
     'isDone': item.isDone
   });
   await newItem.save();
-  console.log('(' + newItem.description + ') is saved!');
+  console.log('(' + newItem.title + ') is saved!');
 }
 
 async function updateItem(item) {
   let oldItem = await Item.findOne({ _id: item._id });
   if (oldItem) {
+    oldItem.title = item.title;
     oldItem.description = item.description;
     oldItem.due = item.due;
     oldItem.isDone = item.isDone;
 
     await oldItem.save();
-    console.log('(' + item.description + ') is updated!');
+    console.log('(' + item.title + ') is updated!');
   } else {
     console.log('the task is not found');
   }
@@ -30,33 +32,21 @@ async function deleteItem(item) {
   const result = await Item.deleteOne({ _id: item._id });
   if (result) {
     if (result.deletedCount) {
-      console.log('(' + item.description + ') is deleted');
+      console.log('(' + item.title + ') is deleted');
     } else {
       console.log('the task is not found, or an error occured');
     }
   }
 }
 
-let items, Item;
+let Item;
 
 // async function getItems() {
 //   items = await Item.find();
 //   console.log('items are ready!')
 // }
 
-function setUpMongo() {
-  const itemSchema = new mongoose.Schema({
-    description: String,
-    due: Date,
-    isDone: Boolean,
-  }, { collection: 'items' });
-
-  itemSchema.index({isDone: 1, due: -1})
-
-  Item = new mongoose.model('Item', itemSchema);
-}
-
-async function getItems() {
+async function setUpMongo() {
   await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
       console.log(`Connected to Mongo!`);
@@ -65,29 +55,44 @@ async function getItems() {
       console.log(`Oh no! Mongo connection error!`);
       console.log(err);
     })
-  items = await Item.find();
+
+  const itemSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    due: Date,
+    isDone: Boolean,
+  }, { collection: 'items' });
+
+  itemSchema.index({ isDone: 1, due: -1 })
+
+  Item = new mongoose.model('Item', itemSchema);
 }
 
-router.get('/', async (req, res) => {
-  await getItems().catch(err => console.log(err));
+router.get('/uncompleted/', async (req, res) => {
+  const uncompletedItems = await Item.find({ isDone: false });
+  res.send(uncompletedItems);
+  console.log('number of uncompleted items: ' + uncompletedItems.length);
+});
 
-  res.send(items);
-  console.log('number of all items: ' + items.length);
+router.get('/completed/', async (req, res) => {
+  const completedItems = await Item.find( {isDone: true} );
+  res.send(completedItems);
+  console.log('number of completed items: ' + completedItems.length);
 });
 
 router.post('/', async (req, res) => {
   await saveNewItem(req.body)
-  res.send('saved new item: ' + req.body.description);
+  res.send('saved new item: ' + req.body.title);
 });
 
 router.put('/', async (req, res) => {
   await updateItem(req.body);
-  res.send('updated: ' + req.body.description);
+  res.send('updated: ' + req.body.title);
 })
 
 router.delete('/', async (req, res) => {
   await deleteItem(req.body);
-  res.send('deleted: ' + req.body.description);
+  res.send('deleted: ' + req.body.title);
 })
 
 setUpMongo();
